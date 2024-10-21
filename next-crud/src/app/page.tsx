34c02ec5179dda8199af64949e773eac74af4d35
Luís Filipe <lufimelo@outlook.com>
2024-10-20 21:20:51 -0300

    Configurando a pasta src

---
+++ next-crud/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center items-center bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900">
+      <span className="text-4xl">Texto</span>
+    </div>
+  );
+}
